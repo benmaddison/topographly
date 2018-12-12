d178043577f4587f826e083d277b6787e2b83125
Ben Maddison <benm@workonline.co.za>
2018-12-12 13:37:05 +0200

    run linter before generating go bindings

--- ybindgen.sh
+++ ybindgen.sh
@@ -3,11 +3,27 @@
 # This should be run from the root of the repo.
 # No attempt is made to fix up relative paths.
 
+YANG_DIR="yang"
+
+# Set up a venv to run the yang linter
+VENV_PATH=".venv"
+echo "Creating venv at $VENV_PATH"
+/usr/bin/env python3 -m venv $VENV_PATH
+echo "Installing pyang"
+$VENV_PATH/bin/pip install -U pip pyang
+echo "Getting openconfig pyang plugin"
+/usr/bin/env wget -O $VENV_PATH/oc-pyang.zip https://github.com/openconfig/oc-pyang/archive/master.zip
+/usr/bin/env unzip $VENV_PATH/oc-pyang.zip -d $VENV_PATH/oc-pyang/
+
+echo "Running oc-pyang linter"
+$VENV_PATH/bin/pyang --plugindir=$VENV_PATH/oc-pyang/oc-pyang-master/openconfig_pyang/plugins/ --lint $YANG_DIR/topology-v*.yang || exit 2
+
 GEN_PATH="vendor/github.com/openconfig/ygot/generator/generator.go"
 GEN_ARGS="-path=yang -generate_delete -generate_getters -generate_fakeroot -fakeroot_name=root -compress_paths"
 
 # find matching yang schemas
-for F in yang/topology-v*.yang; do
+echo "Generating Go bindings"
+for F in $YANG_DIR/topology-v*.yang; do
   # get the major version identifier
   V=$(echo $F | sed -E 's/^.*topology-(v.+)\.yang$/\1/')
   # set the output path for the generated bindings
